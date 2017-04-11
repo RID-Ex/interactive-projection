deaf37f9f7b7dec36f6f03ab0784de7f915bdfbc
Yamashiro <yamashiro7044@gmail.com>
2017-04-11 20:26:22 +0900

    Add

--- Assets/ModeSelection/PlayerEmitter.cs
+++ Assets/ModeSelection/PlayerEmitter.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UniRx;
+using System;
 
 public class PlayerEmitter : NetworkBehaviour {
 
-	public GameObject particlePrefab;
+	public GameObject explosionPrefab;
+	public GameObject fireflyPrefab;
 
 	Camera camera;
 
@@ -17,15 +20,30 @@ public class PlayerEmitter : NetworkBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			CmdFire (Input.mousePosition.x, Input.mousePosition.y);
+			var pos = camera.ScreenToViewportPoint (new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+			CmdFire (pos.x, pos.y);
 		}
 	}
 
 	[Command]
 	public void CmdFire(float x, float y) {
-		var pos = camera.ScreenToWorldPoint (new Vector3 (x, y, -10));
+		var pos = camera.ViewportToWorldPoint (new Vector3 (x, y, -10));
 		pos.z = 0;
-		var particle = Instantiate (particlePrefab, pos, Quaternion.identity);
-		Destroy (particle, 3);
+
+		// firefly
+		var fireflyPos = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 10));
+//		fireflyPos.x = pos.x;
+		var firefly = Instantiate(fireflyPrefab, fireflyPos, Quaternion.identity);
+		var fireflyRigidBody = firefly.GetComponent<Rigidbody2D> ();
+		var d = pos - firefly.transform.position;
+		fireflyRigidBody.velocity = d * 0.3F;
+
+		Observable.Timer (TimeSpan.FromSeconds (3.3)).Subscribe (_ => {
+			var explosion = Instantiate (explosionPrefab, pos, Quaternion.identity);
+			Handheld.Vibrate();
+			Destroy (explosion, 3);
+		});
+
+		Destroy (firefly, 3);
 	}
 }
